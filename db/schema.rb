36bdc1bfd3d8c39e0510d1a9270db671def4dca2
Kaouthar Elherch <kaouthar.elherch@gmail.com>
2024-02-03 12:44:41 +0100

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -38,12 +38,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_03_110611) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
-    t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
