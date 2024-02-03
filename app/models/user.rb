3d4124d8c50603caa9ef33b6029dab024d13c5f0
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-02-03 12:07:18 +0100

    add extra columns to suers table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :address, :phone_number, presence: true
+
+  has_many :robots, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
