597da45f4cc3cc2f689222be0e0fc271c7368388
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-02-03 12:11:09 +0100

    remove validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :address, :phone_number, presence: true
-
   has_many :robots, dependent: :destroy
   has_many :orders, dependent: :destroy
 end
