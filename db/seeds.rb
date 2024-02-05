58aadcb671db4ea80e5d63489c76a847fdddaa0c
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-02-05 18:04:36 +0100

    seeding photos as well as flexbox and styling in robot index

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,47 +25,84 @@ marta = User.create(first_name: "Marta", last_name: "Gray", address: "12 gardens
 
 burgerbot = Robot.new(name: "Burgerbot", category: "cooking", description: "Mouth watering burgers on sight! Be prepared to melt!", price: 263)
 burgerbot.user = richard
+burgerbot_photo = URI.open("https://imageio.forbes.com/blogs-images/christinatroitino/files/2018/06/Creator-Device-Pickle-Tomato-Onion_2-1200x800.jpg?height=474&width=711&fit=bounds")
+burgerbot.photo.attach(io: burgerbot_photo, filename: "burgerbot.png", content_type: "image/png")
 pastabot = Robot.new(name: "Pastabot", category: "cooking", description: "This robot will cook delicious pasta, as if you were sitting in a traditional restaurant in a small italian village. Pick the right wine and enjoy the ride!", price: 313)
 pastabot.user = richard
-pasta_bot_photo = URI.open("https://www.philips.com/c-dam/b2c/category-pages/Household/kitchen-appliances/pasta-maker/EU7/philips-pastmaker-EU7-thumbnail.jpg")
-pastabot.photo.attach(io: pasta_bot_photo, filename: "pastabot.png", content_type: "image/png")
-
+pastabot_photo = URI.open("https://www.philips.com/c-dam/b2c/category-pages/Household/kitchen-appliances/pasta-maker/EU7/philips-pastmaker-EU7-thumbnail.jpg")
+pastabot.photo.attach(io: pastabot_photo, filename: "pastabot.png", content_type: "image/png")
 pizzabot = Robot.new(name: "Pizzabot", category: "cooking", description: "Probaly the most efficient of his generation. Up to 5 pizzas prepared and cooked per minute", price: 154)
 pizzabot.user = richard
+pizzabot_photo = URI.open("https://www.zdnet.com/a/img/resize/9d1b9683af5e8dd44c7bbea0973583a7c3a6df8c/2018/03/15/2d583c07-0951-44ac-9925-5d3a40f93dcf/5aa966e760b22c85d72647c7-1280x7201mar152018140904poster.jpg?auto=webp&fit=crop&height=675&width=1200")
+pizzabot.photo.attach(io: pizzabot_photo, filename: "pizzabot.png", content_type: "image/png")
 donutbot = Robot.new(name: "Donutbot", category: "cooking", description: "It will prepare for you doughnuts with a perfect geometrical form!!", price: 216)
 donutbot.user = richard
+donutbot_photo = URI.open("https://m.media-amazon.com/images/I/71DkTSGE59S.jpg")
+donutbot.photo.attach(io: donutbot_photo, filename: "donutbot.png", content_type: "image/png")
 eggbot = Robot.new(name: "Eggbot", category: "cooking", description: "Eggs benedict, omelette, sunny side up, you name it, it does it all.", price: 230)
 eggbot.user = richard
+eggbot_photo = URI.open("https://cdn.openart.ai/stable_diffusion/c251d0c365c7135c6f28d3240a55ac6b5e02bf8b_2000x2000.webp")
+eggbot.photo.attach(io: eggbot_photo, filename: "eggbot.png", content_type: "image/png")
 vacbot = Robot.new(name: "Vacbot", category: "cleaning", description: "Nothing left on the floor after them.", price: 545)
 vacbot.user = paul
+vacbot_photo = URI.open("https://www.livemint.com/lm-img/img/2024/01/24/600x338/best_robotic_vacuum_cleaner_1706094429597_1706094429964.jpg")
+vacbot.photo.attach(io: vacbot_photo, filename: "vacbot.png", content_type: "image/png")
 swipebot = Robot.new(name: "Swipebot", category: "cleaning", description: "You want it to shine? Let Swipebot do the work!", price: 522)
 swipebot.user = paul
+swipebot_photo = URI.open("https://i.ebayimg.com/images/g/rvIAAOSwdXdlE8EI/s-l1200.jpg")
+swipebot.photo.attach(io: swipebot_photo, filename: "swipebot.png", content_type: "image/png")
 dustbot = Robot.new(name: "Dustbot", category: "cleaning", description: "No more dust. Ever.", price: 620)
 dustbot.user = paul
+dustbot_photo = URI.open("https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/a6c6c76744fe81e5f3e9515254066dd8.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp")
+dustbot.photo.attach(io: dustbot_photo, filename: "dustbot.png", content_type: "image/png")
 dishbot = Robot.new(name: "Dishbot", category: "cleaning", description: "So clean, so efficient. You will be surprised by their performance, and this without breaking anything.", price: 346)
 dishbot.user = paul
+dishbot_photo = URI.open("https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/05/Screenshot-2022-05-25-at-2.11.25-PM.png")
+dishbot.photo.attach(io: dishbot_photo, filename: "dishbot.png", content_type: "image/png")
 laundrybot = Robot.new(name: "Laundrybot", category: "cleaning", description: "Always difficult to sort things out before washing them. This robot knows how to do it!", price: 246)
 laundrybot.user = paul
+laundrybot_photo = URI.open("https://media.istockphoto.com/id/947128288/photo/autonomous-service-robot-is-ironing-some-clothes-aal-cyborg-is-supporting-by-doing-some-work.jpg?s=612x612&w=0&k=20&c=9y2EHwR0RMBrxxRAaX3vGbrt06IHb7cu-mY5lPuEzPM=")
+laundrybot.photo.attach(io: laundrybot_photo, filename: "laundrybot.png", content_type: "image/png")
 treebot = Robot.new(name: "Treebot", category: "gardening", description: "Trees cut in all possible geometrical forms with Treebot!", price: 825)
 treebot.user = joanne
+treebot_photo = URI.open("https://spectrum.ieee.org/media-library/image.jpg?id=25574755&width=980")
+treebot.photo.attach(io: treebot_photo, filename: "treebot.png", content_type: "image/png")
 grassbot = Robot.new(name: "Grassbot", category: "gardening", description: "Mowing the lawn. Pff... A thing of the past! Let the specialist do it for you.", price: 754)
 grassbot.user = joanne
+grassbot_photo = URI.open("https://earimediaprodweb.azurewebsites.net/Api/v1/Multimedia/908171f9-d298-471a-baa6-b8d8a5a66e6f/Rendition/low-res/Content/Public")
+grassbot.photo.attach(io: grassbot_photo, filename: "grassbot.png", content_type: "image/png")
 flowerbot = Robot.new(name: "Flowerbot", category: "gardening", description: "To take care of flowers you need a very delicate, highly specialized bot like this one.", price: 925)
 flowerbot.user = joanne
+flowerbot_photo = URI.open("https://images.squarespace-cdn.com/content/v1/5494b8f5e4b0d7d16a2b75d4/1561996082043-APSI6GPJ7N43BYAXCN5B/florabot-1.png")
+flowerbot.photo.attach(io: flowerbot_photo, filename: "flowerbot.png", content_type: "image/png")
 plantprotectbot = Robot.new(name: "Plant Protect Bot", category: "gardening", description: "No quarter for bugs around here with Plant Protect Bot", price: 650)
 plantprotectbot.user = joanne
+plantprotectbot_photo = URI.open("https://www.aafarmer.co.uk/wp-content/uploads/2020/11/Small-Robot-Company-trials-Tom-1.5-credit-Small-Robot-Company-1.jpg")
+plantprotectbot.photo.attach(io: plantprotectbot_photo, filename: "plantprotectbot.png", content_type: "image/png")
 wateringbot = Robot.new(name: "Wateringbot", category: "gardening", description: "This bot likes water and that is why it knows how to use it without waste.", price: 398)
 wateringbot.user = joanne
+wateringbot_photo = URI.open("https://www.digitaltrends.com/wp-content/uploads/2016/04/growver-2.jpg?resize=2048%2C1152&p=1")
+wateringbot.photo.attach(io: wateringbot_photo, filename: "wateringbot.png", content_type: "image/png")
 dogbot = Robot.new(name: "Dogbot", category: "pet sitting", description: "Doggy likes Dogbot because it knows how to play!", price: 1205)
 dogbot.user = steve
+dogbot_photo = URI.open("https://erepublic.brightspotcdn.com/dims4/default/fdcce84/2147483647/strip/true/crop/1000x521+0+72/resize/840x438!/quality/90/?url=http%3A%2F%2Ferepublic-brightspot.s3.us-west-2.amazonaws.com%2F37%2F94%2F85761bbc4a8e928c7748a81f7fd6%2Fspot.png")
+dogbot.photo.attach(io: dogbot_photo, filename: "dogbot.png", content_type: "image/png")
 catbot = Robot.new(name: "Catbot", category: "pet sitting", description: "Catbot can be sneaky; exactly the right kind of companion for you cat", price: 1562)
 catbot.user = steve
+catbot_photo = URI.open("https://i.etsystatic.com/41305750/r/il/8e3c3d/4680264844/il_fullxfull.4680264844_9vsd.jpg")
+catbot.photo.attach(io: catbot_photo, filename: "catbot.png", content_type: "image/png")
 parrotbot = Robot.new(name: "Parrotbot", category: "pet sitting", description: "Yes, of course, it knows how to talk, and above all talk back!", price: 1998)
 parrotbot.user = steve
+parrotbot_photo = URI.open("https://img.freepik.com/premium-photo/colorful-joyful-parrot-robot-robotic-bird-isolated-white-background_220770-3152.jpg")
+parrotbot.photo.attach(io: parrotbot_photo, filename: "parrotbot.png", content_type: "image/png")
 bunnybot = Robot.new(name: "Bunnybot", category: "pet sitting", description: "For bunnies who like to be followed everywhere and always have a friend to play with", price: 1620)
 bunnybot.user = steve
+bunnybot_photo = URI.open("https://t3.ftcdn.net/jpg/05/52/97/10/360_F_552971051_9jz7Al2rabKzWS44k8ESxiz3O5O9dqQ5.jpg")
+bunnybot.photo.attach(io: bunnybot_photo, filename: "bunnybot.png", content_type: "image/png")
 snakebot = Robot.new(name: "Snakebot", category: "pet sitting", description: "Understanding snakes is not that easy, you should trust the specialists", price: 2996)
 snakebot.user = marta
+snakebot_photo = URI.open("https://technoupdate2017.files.wordpress.com/2017/03/aaeaaqaaaaaaaakaaaaajdlkmtdjywfjltezztutngvjys1hythmltywnweyngy4zmfhzq.jpg")
+snakebot.photo.attach(io: snakebot_photo, filename: "snakebot.png", content_type: "image/png")
 
 [burgerbot,
   pastabot,
