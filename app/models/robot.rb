748a9ed187a78c6566970f45445e9bec0897da94
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-02-06 16:09:25 +0100

    installed pg-search in robot model and robot controller

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -6,4 +6,14 @@ class Robot < ApplicationRecord
   belongs_to :user
   has_many :orders
   has_one_attached :photo
+  include PgSearch::Model
+  pg_search_scope :global_search,
+
+    against: [ :name, :category ],
+    associated_against:{
+      user: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
