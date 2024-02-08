a6b3a71e247c76d97924697e0e9923d0527fd232
Kaouthar Elherch <kaouthar.elherch@gmail.com>
2024-02-08 14:18:01 +0100

    map displayed

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,4 @@
 import { Application } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl'
 
 const application = Application.start()
 
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -6,7 +6,7 @@ class Robot < ApplicationRecord
   belongs_to :user
   has_many :orders
   has_one_attached :photo
-  
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
