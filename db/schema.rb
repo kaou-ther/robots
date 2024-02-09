70dce33f14e1e286df14b797f01b0391dc051d83
Charlotte Bory <cmmbory@gmail.com>
2024-02-09 13:53:39 +0100

    Commit new schema

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_194208) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_robots_on_user_id"
   end
 
